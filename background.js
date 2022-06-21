f4caa2a5931a101de23c681aff41272e0197727c
Fedrosauro <pellizzaro.federico16@gmail.com>
2022-06-22 00:50:02 +0200

    Understanding of how to get name of URL

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if(tab.url && tab.url.includes("")){
+  //get the name of the account
+  if(tab.url && tab.url.includes("twitter.com")){
+    if(tab.url.split("/")[4] === "following")
+      console.log(tab.url.split("/")[3]);
 
+    //chrome.tabs.sendMessage(tabId, {
+      //type: "NEW",
+      //account_id: name
+    //});
   }
 })
--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,12 @@
 (() => {
-   let twitterUnfollowButton;
+   let currentNameAcc = "";
+
+   chrome.runtime.onMessage.addListener((obj, sender, response) => {
+     const { type, name } = obj; //destructioring
+
+     if(type === "NEW"){
+       currentNameAcc = name;
+       newPageLoaded();
+     }
+   })
 })();

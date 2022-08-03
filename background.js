b0d35630944d788d1b11f09cd4dfa2fec313deb3
Fedrosauro <pellizzaro.federico16@gmail.com>
2022-08-04 00:41:06 +0200

    update

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   //get the name of the account
-  if(tab.url && tab.url.includes("twitter.com")){
-    if(tab.url.split("/")[4] === "following")
-      console.log(tab.url.split("/")[3]);
-
-    //chrome.tabs.sendMessage(tabId, {
-      //type: "NEW",
-      //account_id: name
-    //});
+  if(tab.url && tab.url.includes("twitter.com") && tab.url.includes("following")){
+    chrome.tabs.sendMessage(tabId, { tab: tab});
   }
 })
--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,29 @@
 (() => {
-   let currentNameAcc = "";
+  let username;
+  const userRow;
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    var { tab } = obj;
 
-   chrome.runtime.onMessage.addListener((obj, sender, response) => {
-     const { type, name } = obj; //destructioring
+    if(!username) {
+      getNameAccount(); //get the username of the user
+    }
 
-     if(type === "NEW"){
-       currentNameAcc = name;
-       newPageLoaded();
-     }
-   })
+    if(tab.url.split("/")[3] === username){
+      generateButtons();
+    }
+
+  });
+
+  const getNameAccount = () => {
+    username = document.querySelector('a[data-testid=AppTabBar_Profile_Link]').getAttribute('href').substring(1);
+    console.log(username); //testing thing
+  }
+
+  const generateButtons = () => {
+    console.log("generation....");
+
+    const unFollowButton = document.getElementByClassName('css-1dbjc4n r-19u6a5r')[0];
+    userRow = document.getElementByClassName('css-1dbjc4n r-1wbh5a2 r-dnmrzs r-1ny4l3l')[0];  // 0->4 primo utente e cosi via
+  }
+
 })();

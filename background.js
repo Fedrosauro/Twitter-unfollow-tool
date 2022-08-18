fec29dc8fefda67ea69e8ff895845829821812a1
Fedrosauro <pellizzaro.federico16@gmail.com>
2022-08-18 15:27:01 +0200

    need to fix refresh page

--- background.js
+++ background.js
@@ -7,6 +7,6 @@ function twitterTab(tabId, tab){
     let msg = { //create the object we will send back to the tab
       name: tab.url.split("/")[3]
     }
-    chrome.tabs.sendMessage(tabId, msg); //send msg to tab with the id "tabID"    
+    chrome.tabs.sendMessage(tabId, msg); //send msg to tab with the id "tabID"
   }
 }
--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,24 @@
 console.log("Chrome extension started");
 
-let nameAccTab;
-chrome.runtime.onMessage.addListener(messageReceiver);
-
-function messageReceiver(msg, sender, response){
-  nameAccTab = msg.name; //get the name of the acc of the tab you"re in
-  console.log("I've received a message!")
-}
-
-let element, id, targetNode, config, observer;
+let nameAccTab, element, objToAdd, id, targetNode, config, observer, startTab, firstRefresh;
 let myArray = new Array(32);
 
-id = setInterval(getThings, 100);
+startTab = true;
+
+chrome.runtime.onMessage.addListener(tabUpdated);
+
+function tabUpdated(msg, sender, response){
+  nameAccTab = msg.name; //get the name of the acc of the tab you"re in
+  console.log("I've received a message!");
+  firstRefresh = true; //needed when refreshing the page but no children has been added
+  id = setInterval(getThings, 100);
+}
+
+if(startTab){
+  id = setInterval(getThings, 100);
+  console.log("Start tab happened");
+  startTab = false;
+}
 
 function getThings(){
   element = document.querySelector("a[data-testid='AppTabBar_Profile_Link']"); //element for the profile name
@@ -36,34 +43,40 @@ function observationStart(){
   console.log("Observation started");
 
   function observerFunc(mutationList, observer){
+    buttonsGeneration();
+
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
-        console.clear();
-        for(var i = 0; i < myArray.length; i++){
-          myArray[i] = undefined;
-        }
-        var children = targetNode.firstChild.children;
-        for(var i = 0; i < children.length; i++){
-          myArray[i] = children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.firstChild.firstChild.children[1].firstChild.firstChild.href.substring(20);
-          if(children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.children.length < 4){
-            let newElement = document.createElement("p");
-            newElement.innerHTML = "Unfollow: " + myArray[i];
-            newElement.style.color = "white";
-            newElement.style.border = "1px solid";
-            newElement.style.borderColor = "rgb(83, 100, 113)";
-            newElement.style.borderRadius = "9999px";
-            newElement.style.padding = "7px";
-            newElement.style.paddingRight = "16px";
-            newElement.style.paddingLeft = "16px";
-            newElement.style.fontWeight = "bold";
-            newElement.style.fontFamily = "TwitterChirp";
-            newElement.style.fontSize = "14px";
-            let elementToAppend = children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.children[1];
-            elementToAppend.parentNode.insertBefore(newElement, elementToAppend.nextSibling);
-          }
-        }
-        console.log(...myArray);
+        buttonsGeneration();
       }
     }
   }
+}
+
+function buttonsGeneration(){
+  for(var i = 0; i < myArray.length; i++){
+    myArray[i] = undefined;
+  }
+  var children = targetNode.firstChild.children;
+  for(var i = 0; i < children.length; i++){
+    myArray[i] = children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.firstChild.firstChild.children[1].firstChild.firstChild.href.substring(20);
+    if(children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.children.length < 4){
+      objToAdd = document.createElement("p");
+      objToAdd.innerHTML = "Unfollow: " + myArray[i];
+      objToAdd.style.color = "white";
+      objToAdd.style.border = "1px solid";
+      objToAdd.style.borderColor = "rgb(83, 100, 113)";
+      objToAdd.style.borderRadius = "9999px";
+      objToAdd.style.padding = "7px";
+      objToAdd.style.paddingRight = "16px";
+      objToAdd.style.paddingLeft = "16px";
+      objToAdd.style.fontWeight = "bold";
+      objToAdd.style.fontFamily = "TwitterChirp";
+      objToAdd.style.fontSize = "14px";
+      let elementToAppend = children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.children[1];
+      elementToAppend.parentNode.insertBefore(objToAdd, elementToAppend.nextSibling);
+      console.log("Element added");
+    }
+  }
+  console.log(...myArray);
 }

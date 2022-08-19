e9b69f56fac5cc50ee7b5b749e7c259f1482b101
Fedrosauro <pellizzaro.federico16@gmail.com>
2022-08-19 13:21:37 +0200

    update

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,6 @@
 console.log("Chrome extension started");
 
-let nameAccTab, element, objToAdd, id, targetNode, config, observer, startTab, firstRefresh;
+let nameAccTab, element, objToAdd, id, targetNode, config1, observer1, startTab, firstRefresh, lastURL;
 let myArray = new Array(32);
 
 startTab = true;
@@ -10,7 +10,6 @@ chrome.runtime.onMessage.addListener(tabUpdated);
 function tabUpdated(msg, sender, response){
   nameAccTab = msg.name; //get the name of the acc of the tab you"re in
   console.log("I've received a message!");
-  firstRefresh = true; //needed when refreshing the page but no children has been added
   id = setInterval(getThings, 100);
 }
 
@@ -34,17 +33,18 @@ function getThings(){
 }
 
 function observationStart(){
-  config = {
+  console.log("Observation started");
+
+  buttonsGeneration();
+
+  config1 = {
     childList: true //mutations I want to observe
   }
 
-  observer = new MutationObserver(observerFunc); // Create an observer instance linked to the callback function
-  observer.observe(targetNode.firstChild, config); // Start observing the target node for configured mutations
-  console.log("Observation started");
+  observer1 = new MutationObserver(observerFunc1); // Create an observer instance linked to the callback function
+  observer1.observe(targetNode.firstChild, config1); // Start observing the target node for configured mutations
 
-  function observerFunc(mutationList, observer){
-    buttonsGeneration();
-
+  function observerFunc1(mutationList, observer){
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
         buttonsGeneration();
@@ -58,11 +58,12 @@ function buttonsGeneration(){
     myArray[i] = undefined;
   }
   var children = targetNode.firstChild.children;
+  console.log(children);
   for(var i = 0; i < children.length; i++){
     myArray[i] = children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.firstChild.firstChild.children[1].firstChild.firstChild.href.substring(20);
     if(children[i].firstChild.firstChild.firstChild.firstChild.children[1].firstChild.children.length < 4){
       objToAdd = document.createElement("p");
-      objToAdd.innerHTML = "Unfollow: " + myArray[i];
+      objToAdd.innerHTML = "Unfollow";
       objToAdd.style.color = "white";
       objToAdd.style.border = "1px solid";
       objToAdd.style.borderColor = "rgb(83, 100, 113)";

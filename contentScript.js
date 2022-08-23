1af6017fb3629eb922eb9975fae13344c7f47646
Fedrosauro <pellizzaro.federico16@gmail.com>
2022-08-23 14:31:24 +0200

    update

--- contentScript.js
+++ contentScript.js
@@ -79,6 +79,20 @@ function buttonsGeneration(){
 
       objToAdd.classList.add("btn");
 
+      var animateButton = function(e) {
+
+        e.preventDefault;
+        //reset animation
+        e.target.classList.remove("animate");
+
+        e.target.classList.add("animate");
+        setTimeout(function(){
+          e.target.classList.remove("animate");
+        },700);
+      };
+
+      objToAdd.addEventListener("click", animateButton, false);
+
 
       console.log("Element added");
     }
